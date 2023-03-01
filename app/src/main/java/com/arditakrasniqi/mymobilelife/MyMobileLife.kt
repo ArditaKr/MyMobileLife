29ef26499d47a777e22eb6d89200d43e061df35d
Ardita Krasniqi <ardita.krasniqi@keds-energy.com>
2023-03-01 21:51:06 +0100

    Initialize app, creating screens

---
+++ app/src/main/java/com/arditakrasniqi/mymobilelife/MyMobileLife.kt
@@ -0,0 +1,8 @@
+package com.arditakrasniqi.mymobilelife
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyMobileLife: Application() {
+}
---
+++ app/src/main/java/com/arditakrasniqi/mymobilelife/presentation/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.arditakrasniqi.mymobilelife.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.arditakrasniqi.mymobilelife.R
+import com.arditakrasniqi.mymobilelife.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.listFragment,
+                R.id.imageFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
